625c72808f91a89b770e433b0d0a75608bc9d4ed
Panithan Lertsuntivit <149270319+Panithan-Lertsuntivit@users.noreply.github.com>
2024-10-19 15:00:45 -0700

    Update Batch_randomization.m
    Randomizing elements into new batch

--- Batch_randomization.m
+++ Batch_randomization.m
@@ -4,24 +4,31 @@ clear; clc;
 
 %% Initial batch
 
-batches_200 = {'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200',
-    'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200',
-    'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200',
+batches_200 = {'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200';
+    'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200';
+    'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200';
     'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200'};
 
 new_size = 2;
 
+input_batch = batches_200;
+
 %% Creating Function/Algorithm to create a new randomized batch
 
 % Data extraction
-num_elements = numel(batches_200);
+num_elements = numel(input_batch);
 size = abs(new_size);
 
 % Cell Array Dimensions and Creation
 new_idx = randperm(num_elements);
-rows = ceil(new_idx / new_size);
-columns = new_size;
+columns = ceil(num_elements / new_size);
+rows = new_size;
 
 output_batches = cell([rows, columns]);
+
+% Randomizing Elements into New Batch
+for (i = 1:num_elements)
+    output_batches(i) = input_batch(new_idx(i));
+end
 
 
