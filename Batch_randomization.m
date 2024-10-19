03ec4bd05badadebe4404dc0b4ef9a05d944f918
Panithan Lertsuntivit <149270319+Panithan-Lertsuntivit@users.noreply.github.com>
2024-10-19 14:48:39 -0700

    Update - Create New Cell Array
    Method to create a new cell array to store new randomized batches

--- Batch_randomization.m
+++ Batch_randomization.m
@@ -2,11 +2,26 @@
 
 clear; clc;
 
-%% Abbreviations
+%% Initial batch
 
 batches_200 = {'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200',
     'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200',
     'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200',
     'red_200', 'green_200', 'blue_200', 'purple_200', 'black_200'};
+
+new_size = 2;
+
+%% Creating Function/Algorithm to create a new randomized batch
+
+% Data extraction
+num_elements = numel(batches_200);
+size = abs(new_size);
+
+% Cell Array Dimensions and Creation
+new_idx = randperm(num_elements);
+rows = ceil(new_idx / new_size);
+columns = new_size;
+
+output_batches = cell([rows, columns]);
 
 
