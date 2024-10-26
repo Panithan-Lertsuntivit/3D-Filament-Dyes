88a72995e02dcdade8ff06e18b7b0e61993a3c45
Panithan Lertsuntivit <149270319+Panithan-Lertsuntivit@users.noreply.github.com>
2024-10-26 15:26:25 -0700

    Adapted for more functionality

--- Randomize-Prints/Batch_randomization.m
+++ Randomize-Prints/Batch_randomization.m
@@ -22,46 +22,26 @@ batches_230 = {'red_230', 'green_230', 'blue_230', 'purple_230', 'black_230';
     'red_230', 'green_230', 'blue_230', 'purple_230', 'black_230';
     'red_230', 'green_230', 'blue_230', 'purple_230', 'black_230'};
 
-% Randomizing Batches
+%% Randomizing Batches and Saving Results
 new_batches_200 = random_batch_sort(batches_200, num_perplate);
 new_batches_215 = random_batch_sort(batches_215, num_perplate);
 new_batches_230 = random_batch_sort(batches_230, num_perplate);
 
-% Creating file to save results
-fileID = fopen('Printing_Batches.txt', 'w');
+% Combining Batches
+total_batches = [new_batches_200, new_batches_215, new_batches_230];
 
-% Saving 200C Batches to file
-[row_200, column_200] = size(new_batches_200);
+% Creating .csv file and saving results there
+NewBatches_ID = fopen('New_Batches.csv', 'w');
 
-fprintf(fileID, 'Batch Printing at 200C \r\n');
-fprintf(fileID, '%-10s %-15s %-15s \r\n', 'Batch #', 'Object 1', 'Object 2');
+saveorder_csv(total_batches, NewBatches_ID);
 
-for i = 1:column_200
-    fprintf(fileID, '%-10d %-15s %-15s \r\n', i, new_batches_200{1, i}, new_batches_200{2, i});
-end
+%% Randomizing Print Order and Saving Results
+printing_order = random_print_order(total_batches);
 
-% Saving 215C Batches to file
-[row_215, column_215] = size(new_batches_215);
+% Creating .csv file and saving printing order there 
+csvID = fopen('Printing_Order.csv', 'w');
 
-fprintf(fileID, '\r\n \r\nBatch Printing at 215C \r\n');
-fprintf(fileID, '%-10s %-15s %-15s \r\n', 'Batch #', 'Object 1', 'Object 2');
-
-for i = 1:column_215
-    fprintf(fileID, '%-10d %-15s %-15s \r\n', i, new_batches_215{1, i}, new_batches_215{2, i});
-end
-
-% Saving 230C Batches to file
-[row_230, column_230] = size(new_batches_230);
-
-fprintf(fileID, '\r\n \r\nBatch Printing at 230C \r\n');
-fprintf(fileID, '%-10s %-15s %-15s \r\n', 'Batch #', 'Object 1', 'Object 2');
-
-for i = 1:column_230
-    fprintf(fileID, '%-10d %-15s %-15s \r\n', i, new_batches_230{1, i}, new_batches_230{2, i});
-end
-
-% Closing file
-fclose(fileID);
+saveorder_csv(printing_order, csvID);
 
 
 
