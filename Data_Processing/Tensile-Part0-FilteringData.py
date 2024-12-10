9a9def7020b766d45e854e2b49e32a57e546c574
Panithan Lertsuntivit <panithanblue@gmail.com>
2024-12-09 18:16:52 -0800

    Data Filtering for Tensile Data

--- Data_Processing/Tensile-Part0-FilteringData.py
+++ Data_Processing/Tensile-Part0-FilteringData.py
@@ -1,7 +1,6 @@
-# Script is called 3Point-Part0-FilteringData.py
+# Script is called Tensile-Part0-FilteringData.py
 import pandas as pd  # reading and processing data
 import numpy as np
-
 
 '''----- Beginning of Functions ---------------------------------------------'''
 # Function to process and save data for a specific combination
@@ -19,10 +18,9 @@ def process_and_save_data(folder_location, combination, sequence_nums):
 
         combined_data = pd.DataFrame()  # Create empty DataFrame to store combined data
         # Pre Initialize Data Frame headers
-        combined_data['Flex_Stress'] = ''
-        combined_data['Flex_Strain'] = ''
+        combined_data['Stress'] = ''
+        combined_data['Strain'] = ''
         combined_data['YoungModulus'] = ''
-        combined_data['Sequence'] = ''
 
         file_path = f"{folder_location}/Sequence_{seq}.csv"
 
@@ -42,56 +40,21 @@ def process_and_save_data(folder_location, combination, sequence_nums):
                                              errors='coerce')
         df['Load [N]'] = pd.to_numeric(df['Load [N]'], errors='coerce')
 
-        ''' Removing the initial noise - using find_increasing_index() '''
-        # find_increasing_index() takes only np arrays
-        unfiltered_load = df['Load [N]'].to_numpy()
-        unfiltered_extension = df['Extension [mm]'].to_numpy()
-
-        increasing_idx = find_increasing_index(unfiltered_load)
-
-        filtered_load_N = unfiltered_load[increasing_idx:]
-        filtered_deflection_mm = (unfiltered_extension[increasing_idx:] -
-                               unfiltered_extension[increasing_idx])
-
-        '''Calculating Flexural Stress [MPa] and Strain'''
-        # 3 Point Bending Dimensions
-        length_mm = 101.6       # Support Span Length
-        width_mm = 19.05        # Width
-        thickness_mm = 6.35     # Thickness or height
-
-        flex_stress_kPa = ((3 * filtered_load_N * length_mm) /
-                       (2 * width_mm * thickness_mm))
-        flex_stress_MPa = flex_stress_kPa * (pow(10, -3))
-        flex_strain = ((6 * thickness_mm * filtered_deflection_mm) /
-                       pow(length_mm, 2))
+        '''Calculating Stress [MPa] and Strain'''
+        stress_MPa = df['Load [N]'].to_numpy() / 80.645     # Divide by 80.645 mm^2
+        strain = df['Extension [mm]'].to_numpy() / 66.04  # Divide by 127 mm
 
         '''Saving Stress and Strain Data to combined DataFrame'''
-        combined_data['Flex_Stress'] = flex_stress_MPa.tolist()
-        combined_data['Flex_Strain'] = flex_strain.tolist()
+        combined_data['Stress'] = stress_MPa.tolist()
+        combined_data['Strain'] = strain.tolist()
         combined_data.loc[0, 'Sequence'] = seq
         combined_data.loc[0, 'YoungModulus'] \
-            = find_young_modulus(flex_stress_MPa.tolist(), flex_strain.tolist())
+            = find_young_modulus(stress_MPa.tolist(), strain.tolist())
 
-        output_sequence_file = f"3PointBending-FilteredData/Sequence_{seq}_filtered.csv"
+        output_sequence_file = f"Tensile-FilteredData/Sequence_{seq}_filtered.csv"
         combined_data.to_csv(output_sequence_file, index=False)
         print(f'Saved filtered data to: {output_sequence_file}')
 
-
-def find_increasing_index(y_values):
-    # Function finds the first index of an increasing pattern (above the
-    # set tolerance)
-    # Using the rising segment of the values
-    max_idx = np.argmax(y_values)
-    y_values_rising = y_values[0:max_idx]
-
-    tolerance = 0.50
-    less_than_tolerance = np.where(y_values_rising < tolerance)
-
-    # Getting the last index of array
-    # Note: np.where outputs a tuple, so we want the first tuple item (use [0])
-    first_increasing_idx = less_than_tolerance[0][-1]
-
-    return first_increasing_idx
 
 def find_young_modulus(stress_list, strain_list):
     # Function calculates INDIVIDUAL curve property: Young's Modulus
@@ -119,34 +82,27 @@ def find_young_modulus(stress_list, strain_list):
 
 '''Mainline of Code - Beginning'''
 # Defining sequence numbers for each color / temperature combination
-# For 3 Point Bend Tests - Creating a Dictionary
+# For Tensile Tests - Creating a Dictionary
 file_groups = {
     # Red Color Combinations
-    "Red_200": [1, 5, 16, 56], "Red_215": [3, 25, 30, 37],
-    "Red_230": [9, 46, 53, 58],
-    # Green Color Combinations
-    "Green_200": [6, 13, 28, 34], "Green_215": [26, 40, 41, 48],
-    "Green_230": [15, 45, 49, 54],
+    "Red_200": [5, 7, 9, 11], "Red_215": [27, 29, 36, 37],
+    "Red_230": [44, 53, 54, 59],
     # Blue Color Combinations
-    "Blue_200": [7, 31, 39, 47], "Blue_215": [14, 20, 29, 55],
-    "Blue_230": [2, 4, 33, 44],
+    "Blue_200": [8, 12, 14, 19], "Blue_215": [22, 25, 32, 33],
+    "Blue_230": [43, 50, 57, 58],
+    # Green Color Combinations
+    "Green_200": [10, 13, 15, 16], "Green_215": [23, 45, 52, 55],
+    "Green_230": [34, 40, 46, 60],
     # Purple Color Combinations
-    "Purple_200": [12, 22, 23, 59], "Purple_215": [24, 32, 51, 60],
-    "Purple_230": [11, 19, 36, 57],
+    "Purple_200": [4, 6, 17, 18], "Purple_215": [24, 30, 35, 56],
+    "Purple_230": [26, 39, 49, 51],
     # Black Color Combinations
-    "Black_200": [17, 21, 43, 50], "Black_215": [18, 35, 38, 42],
-    "Black_230": [8, 10, 27, 52],
-}
-
-tester_group = {
-    # Red Color Combinations
-    "Blue_230": [2, 4, 33, 44],
-    # "Red_215": [3, 25, 30, 37],
-    # "Red_230": [9, 46, 53, 58],
+    "Black_200": [1, 2, 3, 20], "Black_215": [21, 28, 41, 48],
+    "Black_230": [31, 38, 42, 47],
 }
 
 # Folder name containing CSV files for tensile test results
-data_folder = "3PointBending-Data"
+data_folder = "Tensile-Data"
 
 # Process each color/temperature combination within the file_groups dictionary
 for combination_type, sequences in file_groups.items():
