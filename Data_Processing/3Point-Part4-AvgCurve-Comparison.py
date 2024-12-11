6204d73d8bebc5dd60ccefeea1178a4a13bc8642
Panithan Lertsuntivit <panithanblue@gmail.com>
2024-12-11 00:25:36 -0800

    3 Point Bending Average Curve Comparison

--- Data_Processing/3Point-Part4-AvgCurve-Comparison.py
+++ Data_Processing/3Point-Part4-AvgCurve-Comparison.py
@@ -1,54 +1,54 @@
-# Script is called Tensile-Part5-AverageCurve_Comparison.py
+# Script is called 3Point-Part4-AvgCurve_Comparison.py
 import pandas as pd  # reading and processing data
 import numpy as np
 import matplotlib.pyplot as plt
 
 '''----- Beginning of Functions ---------------------------------------------'''
 
-def stress_strain_from_csv(filelocation):
-    # Function gathers INDIVIDUAL tensile curve stress and strain array
+def flex_stress_strain_from_csv(filelocation):
+    # Function gathers INDIVIDUAL flexural curve stress and strain array
 
     # Reading csv file
     data_frame = pd.read_csv(filelocation)
     header_names = data_frame.columns.tolist()
 
-    stress = data_frame[f'{header_names[0]}'].to_numpy()
-    strain = data_frame[f'{header_names[1]}'].to_numpy()
+    flex_stress = data_frame[f'{header_names[0]}'].to_numpy()
+    flex_strain = data_frame[f'{header_names[1]}'].to_numpy()
 
-    return stress, strain
+    return flex_stress, flex_strain
 
 
-def tensile_curve_properties(array_stress, array_strain):
-    # Function calculates INDIVIDUAL tensile curve properties
-    # Properties: Ultimate Tensile Strength, Young's Modulus, and Yield Strength
+def flex_curve_properties(array_flex_stress, array_flex_strain):
+    # Function calculates INDIVIDUAL flexural curve properties
+    # Properties: Flexural Modulus, and Flexural Yield Strength
 
-    # Calculating the ultimate tensile strength
-    uts_idx = np.argmax(array_stress)
-    uts = array_stress[uts_idx]
-    linear_idx = int(uts_idx / 5)
+    # Calculating the ultimate flexural strength
+    ufs_idx = np.argmax(array_flex_stress)
+    ufs = array_flex_stress[ufs_idx]
+    linear_idx = int(ufs_idx / 5)
 
     # Calculating Young's Modulus
-    linear_stress_segment = array_stress[0:linear_idx]
-    linear_strain_segment = array_strain[0:linear_idx]
+    linear_stress_segment = array_flex_stress[0:linear_idx]
+    linear_strain_segment = array_flex_strain[0:linear_idx]
 
     # Getting linear line of best fit
     [slope, intercept] = np.polyfit(linear_strain_segment,
                                     linear_stress_segment, 1)
-    young_modulus = slope
+    flex_modulus = slope
 
     # Calculating the tensile yield strength
     # 0.2% offset for strain = 0.002 strain
     x_offset = 0.002
-    offset_line = young_modulus * (array_strain - x_offset)
+    offset_line = flex_modulus * (array_flex_strain - x_offset)
 
-    stress_difference = array_stress - offset_line
+    stress_difference = array_flex_stress - offset_line
 
     # np.where outputs a tuple, so we have to pass two index values
     intersect_index = np.where(stress_difference <= 0)[0][0]
 
-    yield_strength = array_stress[intersect_index]
+    flex_yield_strength = array_flex_stress[intersect_index]
 
-    return uts, young_modulus, yield_strength
+    return flex_modulus, flex_yield_strength
 
 
 '''----- End of Functions --------------------------------------------------'''
@@ -79,22 +79,21 @@ color_order = ['black', 'dodgerblue', 'forestgreen', 'darkviolet', 'crimson']
 # Predefine Pandas Dataframe
 avg_curve_props = pd.DataFrame()
 avg_curve_props['Description'] = ''
-avg_curve_props['UTS'] = ''
-avg_curve_props['Young_Modulus'] = ''
-avg_curve_props['Yield_Strength'] = ''
+avg_curve_props['Flexural_Modulus'] = ''
+avg_curve_props['Flex_Yield_Strength'] = ''
 
 counter = 1
 
 ''' Main Code '''
 for temp_group, category in temp_groups.items():
-    # Folder name containing CSV files for tensile test results
-    data_folder = "Tensile-Average-Graphs"
+    # Folder name containing CSV files for flexural test results
+    data_folder = "3Point-Average-Graphs"
 
     # Making Descriptions and Save Paths
     group_temperature = (f"{temp_group}".replace('Temp_','')
                          .replace('C', '°C'))
-    graph_description = f"Tensile Comparison of Colors at {group_temperature}"
-    png_save_path = f"Tensile-Results/Color_Comparison_{temp_group}.png"
+    graph_description = f"3 Point Bending Comparison of Colors at {group_temperature}"
+    png_save_path = f"3Point-Results/Color_Comparison_{temp_group}.png"
 
     # Initializing a figure
     plt.figure(figsize=(10, 6))
@@ -108,25 +107,25 @@ for temp_group, category in temp_groups.items():
         curve_description = (color_temp.replace('_',' ').
                              replace('C', '°C'))
 
-        [current_stress, current_strain] = stress_strain_from_csv(file_path)
+        [current_flexstress, current_flexstrain] = flex_stress_strain_from_csv(file_path)
 
-        [current_uts, current_youngmodulus, current_yieldstrength] \
-            = tensile_curve_properties(current_stress, current_strain)
+        [current_flex_modulus, current_flex_yieldstrength] \
+            = flex_curve_properties(current_flexstress, current_flexstrain)
 
         avg_curve_props.loc[counter, 'Description'] = combination
-        avg_curve_props.loc[counter, 'UTS'] = current_uts
-        avg_curve_props.loc[counter, 'Young_Modulus'] = current_youngmodulus
-        avg_curve_props.loc[counter, 'Yield_Strength'] = current_yieldstrength
+        avg_curve_props.loc[counter, 'Flexural_Modulus'] = current_flex_modulus
+        avg_curve_props.loc[counter, 'Flex_Yield_Strength'] \
+            = current_flex_yieldstrength
 
         counter = counter + 1
 
-        plt.plot(current_strain, current_stress,
+        plt.plot(current_flexstrain, current_flexstress,
                  label=curve_description, color=color_order[i])
 
     # Labeling
     plt.title(graph_description)
-    plt.xlabel("Strain")
-    plt.ylabel("Stress [MPa]")
+    plt.xlabel("Flexural Strain")
+    plt.ylabel("Flexural Stress [MPa]")
     plt.legend()
     plt.grid(True)
 
@@ -137,11 +136,7 @@ for temp_group, category in temp_groups.items():
     plt.show()
 
 # Saving Average Curve Properties to CSV file
-csv_path = f"Tensile-Results/Average_Curve_Properties.csv"
+csv_path = f"3Point-Results/Average_Curve_Flex_Properties.csv"
 avg_curve_props.to_csv(csv_path, index=False)
 
 print(f"Saved csv to: {csv_path}")
-
-
-
-
